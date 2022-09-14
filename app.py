c664ec002004a5b08b20f7c7207b1d85d16f3255
anudeepreddy77 <anudeepreddy1202@gmail.com>
2022-09-14 11:02:28 +0530

    title
    descr

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask,render_template,url_for,request,redirect
+import numpy as np
+import pandas as pd
+import joblib
+import pickle
+
+
+app = Flask(__name__)
+
+model = joblib.load('regressor.pkl')
+onehot = joblib.load('OneHotee.joblib')
+
+
+@app.route('/')
+@app.route('/main')
+def main():
+	return render_template('main.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+	int_features =[[x for x in request.form.values()]]
+	c = ["sex","smoker","region","age","bmi","children"]
+	df = pd.DataFrame(int_features,columns=c)	
+	l = onehot.transform(df.iloc[:,0:3])
+	c = onehot.get_feature_names_out()
+	t = pd.DataFrame(l,columns=c)
+	l2 = df.iloc[:,3:]
+	final =pd.concat([l2,t],axis=1)
+	result = model.predict(final)
+	print("The Result is :",result)
+
+
+	
+
+	return render_template("main.html",prediction_text="Expected Medical Charges in $ : {}".format(result))
+
+
+if __name__ == "__main__":
+	app.debug=True
+	app.run(host = '127.0.1.1', port =8000)
